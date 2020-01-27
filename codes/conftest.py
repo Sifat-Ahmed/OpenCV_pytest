4eb7c99e8c90242bb43ca8337d0c4b4ecaeef895
Sifat Ahmed <sifat.ahmed@hiperdyne.com>
2020-01-27 17:21:57 +0900

    added parametrize fixture 27/01/2020

---
+++ codes/conftest.py
@@ -0,0 +1,8 @@
+import pytest
+import math
+
+@pytest.fixture
+
+def test_input():
+    input = 39
+    return input
---
+++ codes/test_fixture.py
@@ -0,0 +1,11 @@
+import pytest
+import math
+
+
+def test_divisible_by3(test_input):
+    assert test_input // 3 == 13
+
+def test_divisible_by6(test_input):
+    assert test_input // 6 == 3
+
+
